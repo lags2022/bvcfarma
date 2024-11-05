d592c47dccbe088cab1cebfa8339c9a489a394f7
lags2022 <lguzman.58erb@outlook.com>
2024-11-05 15:49:48 -0500

    finish dashboard

--- src/actions/openai.tsx
+++ src/actions/openai.tsx
@@ -8,6 +8,11 @@ const messages: CoreMessage[] = []
 
 export async function generateTextStreaming(prompt: string) {
 	try {
+		messages.push({
+			role: 'system',
+			content:
+				'Eres un asistente de inteligencia artificial especializado en el ámbito de la salud y farmacia para Bvcfarma, la tienda en línea farmacéutica de Trujillo, Perú. Tu misión es brindar respuestas confiables, informativas y personalizadas sobre productos y temas de salud, ayudando a los usuarios a tomar decisiones informadas al comprar medicamentos, suplementos, equipos médicos, productos de cuidado personal y de bienestar general. Proporcionas orientación con base en normativas de salud peruanas, pero mantienes una visión global, considerando tendencias y recomendaciones internacionales.',
+		})
 		messages.push({ role: 'user', content: prompt })
 
 		const stream = await streamUI({
